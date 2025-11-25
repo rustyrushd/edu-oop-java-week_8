2c40436d3674bb7e6f86ea50deb79a9780de4c4b
Rushdius Gluteus Maximus <git@rushd.xyz>
2025-11-25 23:06:37 +0000

    Merge pull request #2
    * Success test for subtract feature
    
    * Failure test for subtract feature

--- src/test/java/ie/atu/CalculatorTest.java
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -24,4 +24,15 @@ public class CalculatorTest {
         Exception ex = assertThrows(ArithmeticException.class, ()-> calc.add(Integer.MAX_VALUE, 1));
         assertEquals("integer overflow", ex.getMessage());
     }
+
+    @Test
+    public void testSubtract_Success() {
+        assertEquals(-2, calc.sub(4, 6));
+    }
+
+    @Test
+    public void testSubtract_Fail() {
+        Exception ex = assertThrows(ArithmeticException.class, ()-> calc.sub(Integer.MIN_VALUE, 1));
+        assertEquals("integer overflow", ex.getMessage());
+    }
 }
