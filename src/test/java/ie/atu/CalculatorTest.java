35b1518cd49d5f2fa590f081b2343bd64a8d4651
rgm <git@rushd.xyz>
2025-11-25 22:55:54 +0000

    Failure test for add feature

--- src/test/java/ie/atu/CalculatorTest.java
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
     Calculator calc;
@@ -16,5 +17,11 @@ public class CalculatorTest {
     @Test
     public void testAdd_Success() {
         assertEquals(10, calc.add(4, 6));
+    }
+
+    @Test
+    public void testAdd_Failure() {
+        Exception ex = assertThrows(ArithmeticException.class, ()-> calc.add(Integer.MAX_VALUE, 1));
+        assertEquals("integer overflow", ex.getMessage());
     }
 }
