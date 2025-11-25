8aa2fb9bf06e8ac254a96594327fec3594a90c46
Rushdius Gluteus Maximus <git@rushd.xyz>
2025-11-25 22:56:20 +0000

    Merge pull request #1
    * Success test for add feature
    
    * Failure test for add feature

--- src/test/java/ie/atu/CalculatorTest.java
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -1,4 +1,27 @@
 package ie.atu;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CalculatorTest {
+    Calculator calc;
+
+    @BeforeEach
+    public void setup() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testAdd_Success() {
+        assertEquals(10, calc.add(4, 6));
+    }
+
+    @Test
+    public void testAdd_Failure() {
+        Exception ex = assertThrows(ArithmeticException.class, ()-> calc.add(Integer.MAX_VALUE, 1));
+        assertEquals("integer overflow", ex.getMessage());
+    }
 }
