2e2b757a7834fcd5b345d0b8c143528cb77a25ef
rgm <git@rushd.xyz>
2025-11-25 22:54:11 +0000

    Success test for add feature

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,7 @@
+package ie.atu;
+
+public class Calculator {
+    public int add(int firstNum, int secondNum) {
+        return Math.addExact(firstNum, secondNum);
+    }
+}
--- src/test/java/ie/atu/CalculatorTest.java
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -1,4 +1,20 @@
 package ie.atu;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator calc;
+
+    @BeforeEach
+    public void setup() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testAdd_Success() {
+        assertEquals(10, calc.add(4, 6));
+    }
 }
