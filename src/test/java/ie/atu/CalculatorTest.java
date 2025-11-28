7355d7c765575d85c2d68731f68c3e3250f5dbab
rgm <git@rushd.xyz>
2025-11-28 16:52:11 +0000

    Separated divide failure tests. Renamed tests according to "UnitOfWork_StateUnderTest_ExpectedBehavior" pattern. Lambda syntax edits

--- src/test/java/ie/atu/CalculatorTest.java
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -15,48 +15,52 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testAdd_Success() {
+    public void add_NoOverflow_ReturnsSum() {
         assertEquals(10, calc.add(4, 6));
     }
 
     @Test
-    public void testAdd_Failure() {
-        Exception ex = assertThrows(ArithmeticException.class, ()-> calc.add(Integer.MAX_VALUE, 1));
+    public void add_Overflow_ThrowsException() {
+        Exception ex = assertThrows(ArithmeticException.class, () -> calc.add(Integer.MAX_VALUE, 1));
         assertEquals("integer overflow", ex.getMessage());
     }
 
     @Test
-    public void testSubtract_Success() {
+    public void subtract_NoOverflow_ReturnsDifference() {
         assertEquals(-2, calc.sub(4, 6));
     }
 
     @Test
-    public void testSubtract_Fail() {
-        Exception ex = assertThrows(ArithmeticException.class, ()-> calc.sub(Integer.MIN_VALUE, 1));
+    public void subtract_Overflow_ThrowsException() {
+        Exception ex = assertThrows(ArithmeticException.class, () -> calc.sub(Integer.MIN_VALUE, 1));
         assertEquals("integer overflow", ex.getMessage());
     }
 
     @Test
-    public void testMultiply_Success() {
+    public void multiply_NoOverflow_ReturnsMultiplication() {
         assertEquals(24, calc.mul(4, 6));
     }
 
     @Test
-    public void testMultiply_Fail() {
-        Exception ex = assertThrows(ArithmeticException.class, ()-> calc.mul(Integer.MIN_VALUE, -1));
+    public void multiply_Overflow_ThrowsException() {
+        Exception ex = assertThrows(ArithmeticException.class, () -> calc.mul(Integer.MIN_VALUE, -1));
         assertEquals("integer overflow", ex.getMessage());
     }
 
     @Test
-    public void testDivide_Success() {
+    public void divide_NoOverflow_ReturnsDivision() {
         assertEquals(2, calc.div(4, 2));
     }
 
     @Test
-    public void testDivide_Fail() {
-        Exception ex1 = assertThrows(ArithmeticException.class, ()-> calc.div(Integer.MIN_VALUE, -1));
-        assertEquals("integer overflow", ex1.getMessage());
-        Exception ex2 = assertThrows(ArithmeticException.class, ()-> calc.div(Integer.MIN_VALUE, 0));
-        assertEquals("/ by zero", ex2.getMessage());
+    public void divide_Overflow_ThrowsException() {
+        Exception ex = assertThrows(ArithmeticException.class, () -> calc.div(Integer.MIN_VALUE, -1));
+        assertEquals("integer overflow", ex.getMessage());
+    }
+
+    @Test
+    public void divide_byZero_ThrowsException() {
+        Exception ex = assertThrows(ArithmeticException.class, () -> calc.div(Integer.MIN_VALUE, 0));
+        assertEquals("/ by zero", ex.getMessage());
     }
 }
